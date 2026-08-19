3a6edefba6f287e7e3b211f17e90595b25fc34b7
gitPrakruti <prakrutim06@gmail.com>
2026-08-19 23:26:01 +0530

    after deployment of backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = (import.meta.env['VITE_API_URL'] as string | undefined) ?? "http://localhost:8000/api";
+const BASE_URL = (import.meta.env['VITE_API_URL'] as string | undefined) ?? "https://smart-filter-xdnf.onrender.com";
 
 const TOKEN_KEY = "smartfilter-token";
 
